1a35e44ce02e82be03c1365742f10035097a18a4
Akshay7016 <asajgule@gmail.com>
2023-06-04 18:56:28 +0530

    Responsiveness added for Footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -25,9 +25,9 @@ const Footer = () => {
 
     return (
         <div className='bg-richblack-800'>
-            <div className='w-11/12 mx-auto flex justify-between items-start text-richblack-400 pt-14 pb-5 border-b-[1px] border-richblack-700'>
+            <div className='w-[85%] lg:w-11/12 mx-auto flex flex-col lg:flex-row gap-12 lg:gap-0 justify-between items-start text-richblack-400 pt-14 pb-5 border-b-[1px] border-richblack-700'>
                 {/* Left container */}
-                <div className='w-[50%] flex flex-row gap-14'>
+                <div className='lg:w-[50%] flex flex-row flex-wrap gap-14'>
                     {/* section 1 */}
                     <div className='flex flex-col gap-4'>
                         {/* Logo */}
@@ -153,7 +153,7 @@ const Footer = () => {
                 </div>
 
                 {/* Right container */}
-                <div className='w-[50%] pl-[30px] pb-[20px] flex flex-row justify-between border-l-[1px] border-richblack-700'>
+                <div className='lg:w-[50%] lg:pl-[40px] lg:pb-[20px] flex flex-row flex-wrap  gap-9 lg:gap-14 lg:border-l-[1px] border-richblack-700'>
                     {/* Section 1 - Subjects */}
                     <div className='flex flex-col gap-[10px]'>
                         <div className='font-semibold text-richblack-100'>
@@ -222,7 +222,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className='w-11/12 mx-auto flex justify-between my-12'>
+            <div className='w-11/12 mx-auto flex flex-col gap-2 lg:gap-0 lg:flex-row items-center justify-between my-8 lg:my-12'>
                 <div className='flex flex-row'>
                     {
                         BottomFooter.map((element, index) => {
@@ -239,7 +239,7 @@ const Footer = () => {
                 </div>
 
                 <div className="flex items-center gap-1 text-[13px] font-normal text-richblack-400">
-                    Made with <AiFillHeart className='text-[15px] text-[#FF0000]' /> Akshay &copy; 2023 StudyWeb 
+                    Made with <AiFillHeart className='text-[15px] text-[#FF0000]' /> Akshay &copy; 2023 StudyWeb
                 </div>
             </div>
         </div>
