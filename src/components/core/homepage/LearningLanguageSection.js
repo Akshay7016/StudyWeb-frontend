5ddf51e62b8a4055aeaead5dd0f685f2d3e8648b
Akshay7016 <asajgule@gmail.com>
2023-06-12 17:42:30 +0530

    LearningLanguageSection updated

--- src/components/core/homepage/LearningLanguageSection.js
+++ src/components/core/homepage/LearningLanguageSection.js
@@ -9,35 +9,35 @@ import plan_your_lesson from "../../../assets/Images/Plan_your_lessons.svg";
 
 const LearningLanguageSection = () => {
     return (
-        <div className='mt-[155px] lg:mt-[200px] mb-[70px] flex flex-col items-center gap-7 lg:gap-3'>
-            <div className='text-4xl font-semibold text-center'>
+        <div className='mt-[155px] mb-[40px] lg:mt-[200px] lg:mb-[70px] flex flex-col items-center'>
+            <div className='text-4xl font-semibold text-center mb-3'>
                 Your swiss knife for <HighlightText text="learning any language" />
             </div>
 
-            <div className='lg:w-[75%] mx-auto text-base text-center text-richblack-600 '>
+            <div className='lg:w-[75%] mx-auto text-base text-center text-richblack-600 mb-3'>
                 Using spin making learning multiple languages easy. with 20+ languages realistic voice-over, progress tracking, custom schedule and more.
             </div>
 
-            <div className='w-full flex flex-col lg:flex-row items-center justify-center'>
+            <div className='w-full flex flex-row flex-wrap items-center justify-center'>
                 <img
                     src={know_your_progress}
                     alt="know_your_progress"
-                    className='h-[400px] lg:-mr-32'
+                    className='h-[250px] md:h-[400px] lg:-mr-32'
                 />
                 <img
                     src={compare_with_others}
                     alt="compare_with_others"
-                    className='h-[500px] -mt-[150px] -mb-[200px] md:-mt-14 md:-mb-20 lg:my-0'
+                    className='h-[300px] md:h-[500px]'
                 />
 
                 <img
                     src={plan_your_lesson}
                     alt="plan_your_lesson"
-                    className='h-[450px] lg:-ml-36'
+                    className='h-[300px] md:h-[450px] lg:-ml-36'
                 />
             </div>
 
-            <div className='-mt-[60px] md:-mt-[30px] lg:mt-0'>
+            <div className='mt-[25px] md:mt-[10px] lg:mt-0'>
                 <Button active={true} linkTo="/signup">
                     <div className='font-semibold'>
                         Learn More
