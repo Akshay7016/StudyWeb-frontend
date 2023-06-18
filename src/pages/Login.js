47c23dcab75f01ba8cdbde53afc0ee9ec9eb7575
Akshay7016 <asajgule@gmail.com>
2023-06-18 19:56:48 +0530

    loading/spinner added for login and signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,17 @@
+import { useSelector } from "react-redux";
+
 import Template from "../components/core/auth/Template";
 import loginImg from '../assets/Images/login.webp';
+import Spinner from "../components/common/Spinner";
+
 
 const Login = () => {
+    const { loading } = useSelector((state) => state.auth);
+
+    if (loading) {
+        return <Spinner />
+    };
+
     return (
         <Template
             title="Welcome Back"
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,17 @@
+import { useSelector } from "react-redux";
+
 import Template from '../components/core/auth/Template';
 import signupImage from '../assets/Images/signup.webp';
+import Spinner from "../components/common/Spinner";
+
 
 const Signup = () => {
+    const { loading } = useSelector((state) => state.auth);
+
+    if (loading) {
+        return <Spinner />
+    };
+
     return (
         <Template
             title="Join the millions learning to code with StudyWeb for free"
