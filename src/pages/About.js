a8b780350156c6fc7e30b5ccbd47247b4a58b425
Akshay7016 <asajgule@gmail.com>
2023-06-19 23:29:44 +0530

    Section 1 updated of About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,9 +10,9 @@ const About = () => {
     return (
         <div>
             {/* Section 1 */}
-            <section className='w-full h-[calc(100vh-56px)] bg-richblack-700'>
-                <div className='w-11/12 mx-auto relative'>
-                    <h1 className='w-full md:w-[70%] pt-[30px] md:pt-[40px] lg:pt-[50px] mx-auto text-[28px] md:text-[34px] leading-[40px] text-white text-center font-bold'>
+            <section className='w-full bg-richblack-700'>
+                <div className='w-11/12 mx-auto pt-[30px] md:pt-[40px] lg:pt-[50px] pb-[20px] md:pb-[30px]'>
+                    <h1 className='w-full md:w-[70%] mx-auto text-[28px] md:text-[34px] leading-[40px] text-white text-center font-bold'>
                         Driving Innovation in Online Education for a <HighlightText text="Brighter Future" />
                     </h1>
 
@@ -20,22 +20,25 @@ const About = () => {
                         StudyWeb is at the forefront of driving innovation in online education. We're passionate about creating a brighter future by offering cutting-edge courses, leveraging emerging technologies, and nurturing a vibrant learning community.
                     </p>
 
-                    <div className='w-full mt-[50px] flex gap-6 md:gap-2 lg:gap-5 flex-row flex-wrap justify-center items-center'>
+                    <div className='w-full mt-[30px] md:mt-[45px] flex gap-5 md:gap-0 flex-row flex-wrap md:flex-nowrap justify-center md:justify-between items-center'>
                         <img
                             src={BannerImage1}
                             alt="banner"
+                            loading='lazy'
                             className='w-[46%] md:w-[32%]'
                         />
 
                         <img
                             src={BannerImage2}
                             alt="banner"
+                            loading='lazy'
                             className='w-[46%] md:w-[32%]' />
 
                         <img
                             src={BannerImage3}
                             alt="banner"
-                            className='w-[45%] md:w-[32%]' />
+                            loading='lazy'
+                            className='w-[46%] md:w-[32%]' />
                     </div>
                 </div>
             </section>
