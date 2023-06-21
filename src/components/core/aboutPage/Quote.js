87e5dace0184061670233cfc9aa879759ccf6b10
Akshay7016 <asajgule@gmail.com>
2023-06-21 21:21:28 +0530

    Section 2 and 3 created of About Page

---
+++ src/components/core/aboutPage/Quote.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import HighlightText from '../homepage/HighlightText'
+
+const Quote = () => {
+    return (
+        <div className='w-[82%] mx-auto text-center text-3xl font-bold text-white'>
+            We are passionate about revolutionizing the way we learn. Our innovative platform {" "}
+            <HighlightText text="combines technology" />
+            ,{" "}
+            <span className='bg-gradient-to-b from-[#FF512F] to-[#F09819] text-transparent bg-clip-text'>
+                expertise
+            </span>
+            , and community to create an {" "}
+            <span className='bg-gradient-to-b from-[#E65C00] to-[#F9D423] text-transparent bg-clip-text'>
+                unparalleled educational experience.
+            </span>
+        </div>
+    )
+}
+
+export default Quote
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import HighlightText from "../components/core/homepage/HighlightText";
+import Quote from '../components/core/aboutPage/Quote';
 
 import BannerImage1 from "../assets/Images/aboutus1.webp";
 import BannerImage2 from "../assets/Images/aboutus2.webp";
 import BannerImage3 from "../assets/Images/aboutus3.webp";
+import FoundingStory from "../assets/Images/FoundingStory.png";
 
 const About = () => {
     return (
@@ -46,8 +48,61 @@ const About = () => {
             </section>
 
             {/* Section 2 */}
-            <section className='w-full h-[500px] bg-richblack-900'>
+            <section className='bg-richblack-900'>
+                <div className='mt-[120px] md:mt-[140px] pb-[80px] border-b-[1px] border-richblack-700'>
+                    <Quote />
+                </div>
+            </section>
 
+            {/* Section 3 */}
+            <section className='w-11/12 mx-auto text-white'>
+                {/* sub section 1 */}
+                <div className='mt-[80px] flex gap-16 md:gap-0 flex-col md:flex-row justify-between items-center'>
+                    <div className='w-[90%] md:w-[48%] flex flex-col gap-6'>
+                        <h1 className='text-[34px] font-semibold bg-gradient-to-br from-[#833AB4] via-[#FD1D1D] to-[#FCB045] bg-clip-text text-transparent'>
+                            Our Founding Story
+                        </h1>
+
+                        <p className='text-richblack-300 leading-[21px]'>
+                            Our e-learning platform was born out of a shared vision and passion for transforming education. It all began with a group of educators, technologists, and lifelong learners who recognized the need for accessible, flexible, and high-quality learning opportunities in a rapidly evolving digital world.
+                        </p>
+
+                        <p className='text-richblack-300 leading-[21px]'>
+                            As experienced educators ourselves, we witnessed firsthand the limitations and challenges of traditional education systems. We believed that education should not be confined to the walls of a classroom or restricted by geographical boundaries. We envisioned a platform that could bridge these gaps and empower individuals from all walks of life to unlock their full potential.
+                        </p>
+                    </div>
+
+                    <div className='w-[90%] md:w-[40%]'>
+                        <img
+                            src={FoundingStory}
+                            alt="Founding story"
+                            className='shadow-[0_0_20px_0] shadow-[#FC6767]'
+                        />
+                    </div>
+                </div>
+
+                {/* sub section 2 */}
+                <div className='mt-[80px] md:mt-[120px] mb-[70px] md:mb-[90px] flex flex-col md:flex-row gap-12 md:gap-0 justify-between items-center md:items-start'>
+                    <div className='w-[90%] md:w-[38%] flex flex-col gap-7'>
+                        <h1 className='text-center md:text-left text-[34px] font-semibold bg-gradient-to-b from-[#FF512F] to-[#F09819] bg-clip-text text-transparent'>
+                            Our Vision
+                        </h1>
+
+                        <p className='text-richblack-300 leading-[21px]'>
+                            With this vision in mind, we set out on a journey to create an e-learning platform that would revolutionize the way people learn. Our team of dedicated experts worked tirelessly to develop a robust and intuitive platform that combines cutting-edge technology with engaging content, fostering a dynamic and interactive learning experience.
+                        </p>
+                    </div>
+
+                    <div className='w-[90%] md:w-[38%] flex flex-col gap-7'>
+                        <h1 className='text-center md:text-left text-[34px] font-semibold bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text'>
+                            Our Mission
+                        </h1>
+
+                        <p className='text-richblack-300 leading-[21px]'>
+                            Our mission goes beyond just delivering courses online. We wanted to create a vibrant community of learners, where individuals can connect, collaborate, and learn from one another. We believe that knowledge thrives in an environment of sharing and dialogue, and we foster this spirit of collaboration through forums, live sessions, and networking opportunities.
+                        </p>
+                    </div>
+                </div>
             </section>
         </div>
     )
