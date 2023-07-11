ea9327c48eedc3095bcb1def63076a4423eeb29d
Akshay7016 <asajgule@gmail.com>
2023-07-11 12:53:57 +0530

    className prop added to Button component

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ children, onClick, variant = "variant1", disabled = false, type = "button" }) => {
+const Button = ({ children, onClick, className, variant = "variant1", disabled = false, type = "button" }) => {
     const buttonColor = {
         variant1: "bg-yellow-50 text-richblack-900",
         variant2: "bg-richblack-700 text-richblack-50",
@@ -12,7 +12,7 @@ const Button = ({ children, onClick, variant = "variant1", disabled = false, typ
             onClick={onClick}
             disabled={disabled}
             type={type}
-            className={`${buttonColor[variant]} py-2 px-5 font-semibold rounded-md`}
+            className={`${buttonColor[variant]} py-2 px-5 font-semibold rounded-md ${className}`}
         >
             {children}
         </button>
