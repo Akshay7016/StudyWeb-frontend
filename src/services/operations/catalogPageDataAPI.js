39e4225b0ac53dddc120aeda59c09af2733a8d4b
Akshay7016 <asajgule@gmail.com>
2023-08-01 21:20:39 +0530

    catalogPageDataAPI added

---
+++ src/services/operations/catalogPageDataAPI.js
@@ -0,0 +1,28 @@
+import { toast } from "react-hot-toast";
+
+import { apiConnector } from "../apiConnector";
+import { catalogDataEndpoints } from "../apis";
+
+const { CATALOG_PAGE_DATA_API } = catalogDataEndpoints;
+
+// get catalog page data
+export const getCatalogPageData = async (categoryId) => {
+    const toastId = toast.loading("Loading...");
+    let result = [];
+
+    try {
+        const response = await apiConnector(
+            "GET",
+            CATALOG_PAGE_DATA_API,
+            {
+                categoryId
+            }
+        );
+
+        result = response.data?.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.message);
+    }
+    toast.dismiss(toastId);
+    return result;
+};
