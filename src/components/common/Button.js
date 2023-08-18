5b805eb6d5b3a02f44776c866ee22305dec32cfc
Akshay7016 <asajgule@gmail.com>
2023-08-18 20:47:39 +0530

    Button prop updated

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({ children, onClick, className, variant = "variant1", disabled = false, type = "button" }) => {
+const Button = ({ children, onClick, className = "", variant = "variant1", disabled = false, type = "button" }) => {
     const buttonColor = {
         variant1: "bg-yellow-50 text-richblack-900",
         variant2: "bg-richblack-700 text-richblack-50",
