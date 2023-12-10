2303c2aefb92881f55fd91d592895f13a988455a
Akshay7016 <asajgule@gmail.com>
2023-12-10 16:40:41 +0530

    CourseDetails page made responsive for mobile screens

--- src/pages/CourseDetails.js
+++ src/pages/CourseDetails.js
@@ -82,11 +82,19 @@ export const CourseDetails = () => {
     return (
         <div>
             <div className="bg-richblack-800">
-                <div className="relative max-w-maxContent mx-auto px-2 flex flex-col gap-4 py-32 text-richblack-5">
-                    <p className="w-[48%] text-4xl font-bold text-richblack-5">
+                <div className="relative max-w-maxContent mx-auto px-2 flex flex-col gap-4 pt-6 pb-10 md:py-32 text-richblack-5">
+                    <div className="md:absolute top-20 right-5">
+                        <CourseDetailsCard
+                            courseData={courseData}
+                            setConfirmationModal={setConfirmationModal}
+                            handleBuyCourse={handleBuyCourse}
+                        />
+                    </div>
+
+                    <p className="md:w-[48%] mt-5 md:mt-0 text-4xl font-bold text-richblack-5">
                         {courseData?.courseDetails?.courseName}
                     </p>
-                    <p className="w-[48%] text-richblack-200 text-lg">
+                    <p className="md:w-[48%] text-richblack-200 text-lg">
                         {courseData?.courseDetails?.courseDescription}
                     </p>
                     <div className="text-lg flex items-center gap-2 flex-wrap">
@@ -100,16 +108,16 @@ export const CourseDetails = () => {
                             starSpacing="1px"
                         />
                         <p className="pt-1">{`(${courseData?.courseDetails?.ratingAndReviews?.length} reviews)`}</p>
-                        <p className="pt-1">{`${courseData?.courseDetails?.studentsEnrolled?.length} students enrolled`}</p>
+                        <p className="md:pt-1">{`${courseData?.courseDetails?.studentsEnrolled?.length} students enrolled`}</p>
                     </div>
                     <p className="text-lg">
                         {`Created By
                     ${courseData?.courseDetails?.instructor?.firstName}
                     ${courseData?.courseDetails?.instructor?.lastName}`}
                     </p>
-                    <div className="text-lg flex gap-5 items-center">
-                        <div className="flex items-center gap-2">
-                            <IoMdInformationCircleOutline size={19} />
+                    <div className="text-lg flex flex-col md:flex-row gap-5 md:items-center">
+                        <div className="flex md:items-center gap-2">
+                            <IoMdInformationCircleOutline size={19} className="mt-1 md:mt-0" />
                             <p>
                                 Created at {
                                     formatDate(courseData?.courseDetails?.createdAt)
@@ -121,21 +129,13 @@ export const CourseDetails = () => {
                             English
                         </div>
                     </div>
-
-                    <div className="absolute top-20 right-5">
-                        <CourseDetailsCard
-                            courseData={courseData}
-                            setConfirmationModal={setConfirmationModal}
-                            handleBuyCourse={handleBuyCourse}
-                        />
-                    </div>
                 </div>
             </div>
 
             <div className="bg-richblack-900">
-                <div className="max-w-maxContent mx-auto px-2 text-richblack-5 mb-16">
+                <div className="max-w-maxContent mx-auto px-2 text-richblack-5 mb-10">
                     <div className="max-w-maxContentTab">
-                        <div className="my-8 border border-richblack-600 p-6">
+                        <div className="my-8 border border-richblack-600 p-4 md:p-6">
                             <p className="text-3xl font-semibold mb-5">What you'll learn</p>
                             <p>{courseData?.courseDetails?.whatYouWillLearn}</p>
                         </div>
@@ -144,7 +144,7 @@ export const CourseDetails = () => {
                         <div>
                             <div className="text-[28px] font-semibold mb-3">Course Content</div>
                             <div className="flex justify-between items-center mb-4">
-                                <div className="flex items-center gap-3">
+                                <div className="flex flex-wrap items-center gap-3">
                                     <p>{courseData?.courseDetails?.courseContent?.length} section(s)</p>
                                     <p>{totalNoOfLectures} lecture(s)</p>
                                     <p>{courseData?.totalDuration} total length</p>
